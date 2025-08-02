edcfdf79332663ec9659d7bcff127a20d525e8b1
Emma Brown <emma.b@comcast.net>
2025-08-02 13:57:52 -0400

    Initial Commit

---
+++ Game Solver/Game_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_SolverApp.swift
+//  Game Solver
+//
+//  Created by Emma Brown on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Game_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game SolverTests/Game_SolverTests.swift
@@ -0,0 +1,16 @@
+//
+//  Game_SolverTests.swift
+//  Game SolverTests
+//
+//  Created by Emma Brown on 8/2/25.
+//
+
+import Testing
+
+struct Game_SolverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
